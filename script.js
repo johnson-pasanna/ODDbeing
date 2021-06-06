54f80497eb4debb8afff53e53851df1853ee2547
Johnson Pasanna <85098486+johnson-pasanna@users.noreply.github.com>
2021-06-06 22:56:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// // greetings when user visits the profile page
+
+// var today = new Date(); //create a new data object
+// var hourNow = today.getHours(); //find the current hour
+// var greetings;
+// // display the appropriate greetings based on the current time
+
+// if (hourNow > 18) {
+//     greetings = 'Good evening';
+// }
+// else if (hourNow > 12) {
+//     greetings = 'Good afternoon';
+// }
+// else if (hourNow > 0) {
+//     greetings = 'Good morning';
+// }
+// else {
+//     greetings = 'welcome';
+// }
+// document.write(greetings);
+
+
+// var x = "16" + "blue"; // two strings  //16blue
+// console.log(x);
+
+// var x = 16 + "blue"; // one number one string //16blue
+// console.log(x);
+
+// var x = true + "blue"; //boolean and string  //trueblue
+// console.log(x);
+
+// var x = 16 + 4 + "blue"; // 20blue
+// console.log(x);
+
+// var x = "blue" + 16 + 4; // blue164
+// console.log(x);
+
+// // dynamic types
+// var x;
+// var x = 5;
+// var x = "john";
+// var x = true;
+
+// function squareSum(numbers) {
+//     return numbers.reduce(function (sum, n) {
+//         return n * n + sum;
+//     }, 0);
+// }
+
+// squareSum(2, 3, 4);
+// console.log(squareSum)
+
+console.log('testing');
+
+
